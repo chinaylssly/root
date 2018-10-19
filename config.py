6623ebb92eba240cf6eee31ae4bc9e67f2f3adb9
chinaylssly <chinaylssly@qq.com>
2018-10-19 14:11:57 +0800

    change config

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ import os
 system=platform.system()
 
 headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/5"}
+per_requests_time=1
+##每个请求的延迟
